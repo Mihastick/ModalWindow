f2201ba7cfeb4693a90c8e3d4e8f3f20ae35f64e
Mihastick <savilov990@gmail.com>
2021-11-09 16:14:33 +0500

    third

--- second/script.js
+++ second/script.js
@@ -11,7 +11,17 @@ let but2 = document.getElementById('but2');
 let yourName = document.getElementById('yourName');
 yourName.style.display = 'none';
 
+    function onLoad(){
+        if(localStorage.getItem("name") !== null){
+            yourName.textContent = localStorage.getItem('name');
+            yourName.style.display = 'block';
+            but.textContent = 'Log Out';
+        }
+    }
 
+        window.onload = onLoad();
+
+    
 but.addEventListener('click', show);
 
 function show() {
@@ -23,16 +33,17 @@ function show() {
         yourName.style.display = 'none';
         modal.style.display = 'none';
         but.textContent = 'Sign In';
+        localStorage.removeItem('name');
     }
 }
 
 but2.addEventListener('click', () => {
-    localStorage.setItem('password', passInput.value);
+    // localStorage.setItem('password', passInput.value);
     localStorage.setItem('name', nameInput.value);
     close();
     yourName.textContent = localStorage.getItem('name');
     yourName.style.display = 'block';
-    but.textContent = 'Log Out'
+    but.textContent = 'Log Out';
 });
 
 closeSimb.addEventListener('click', close);
